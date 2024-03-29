beb1b71d27c4a027dc4e6a840ef29c7cb6eef123
marcinoles87 <marcinoles87@gmail.com>
2024-03-29 19:33:44 +0100

    add pages and components

--- src/App.js
+++ src/App.js
@@ -5,13 +5,35 @@ import Main from './components/Main'
 import Footer from './components/Footer';
 import Navabar from './components/Navabar';
 
+import Historia from './pages/Historia'
+
+import { Route, Router, Routes , Link } from 'react-router-dom';
+
+
 function App() {
   return (
+
+
     <div className="app-container">
-      <Header></Header>
-      <Navabar></Navabar>
-      <Main></Main>
-      <Footer></Footer>
+
+      <Routes>
+        <Route path='*' element={
+          <>
+            <Header></Header>
+            <Navabar></Navabar>
+            <Main></Main>
+            <Footer></Footer>
+          </>
+        }>
+
+        </Route>
+
+        <Route path='/historia' element={<Historia></Historia>}></Route>
+        <Route path='/kadra' element={<Historia></Historia>}></Route>
+        <Route path='/Wyniki' element={<Historia></Historia>}></Route>
+        <Route path='/Kontakt' element={<Historia></Historia>}></Route>
+      </Routes>
+      
       
       
       
--- src/components/Navabar.js
+++ src/components/Navabar.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Navabar() {
   return (
     <div className='navbar-container'>
         <ul className='navbar-list'>
-            <li className='navbar-item'><a href='#'>Historia</a></li>
-            <li className='navbar-item'><a href='#'>Kadra</a></li>
-            <li className='navbar-item'><a href='#'>Wyniki</a></li>
-            <li className='navbar-item'><a href='#'>Kontakt</a></li>
+            <Link to={'/historia'}><li className='navbar-item'><a href='#'>Historia</a></li></Link>
+            <Link to={'/kadra'}><li className='navbar-item'><a href='#'>Kadra</a></li></Link>
+            <Link to={'/wyniki'}><li className='navbar-item'><a href='#'>Wyniki</a></li></Link>
+            <Link to={'/kontakt'}><li className='navbar-item'><a href='#'>Kontakt</a></li></Link>
+            
         </ul>
     </div>
   )
