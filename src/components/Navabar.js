06dee39f4bcfdf7149401db001fbbe3ce6a4fab8
marcinoles87 <marcinoles87@gmail.com>
2024-03-30 11:09:01 +0100

    add pages and components

--- src/components/Navabar.js
+++ src/components/Navabar.js
@@ -5,10 +5,10 @@ function Navabar() {
   return (
     <div className='navbar-container'>
         <ul className='navbar-list'>
-            <Link to={'/historia'} className='navbar-link'><li className='navbar-item'>Historia</li></Link>
-            <Link to={'/kadra'} className='navbar-link'><li className='navbar-item'>Kadra</li></Link>
-            <Link to={'/wyniki'}className='navbar-link' ><li className='navbar-item'>Wyniki</li></Link>
-            <Link to={'/kontakt'} className='navbar-link'><li className='navbar-item'>Kontakt</li></Link>
+            <Link to={'/historia'} className='navbar-link-historia'><li className='navbar-item'><p>Historia</p></li></Link>
+            <Link to={'/kadra'} className='navbar-link-kadra'><li className='navbar-item'>Kadra</li></Link>
+            <Link to={'/wyniki'}className='navbar-link-wyniki' ><li className='navbar-item'>Wyniki</li></Link>
+            <Link to={'/kontakt'} className='navbar-link-kontakt'><li className='navbar-item'>Kontakt</li></Link>
             
         </ul>
     </div>
--- src/pages/Historia.js
+++ src/pages/Historia.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 function Historia() {
   return (
-    <div className='historia-container'>Historia</div>
+    <div className='historia-container'>
+      Historia</div>
   )
 }
 
