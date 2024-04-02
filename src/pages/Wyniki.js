da6df32ba042bd19dab5a9842927557af2ecfd2e
marcinoles87 <marcinoles87@gmail.com>
2024-04-02 15:43:04 +0200

    add pages and components

--- src/pages/Wyniki.js
+++ src/pages/Wyniki.js
@@ -6,28 +6,83 @@ import '../App.css'
 function Wyniki() {
 
   const wyniki = [
-    { mecz : 'Grębałowianka - Cracovia  wynik :'} , 
-    { mecz : 'Grębałowianka - Cracovia  wynik :'} , 
-    { mecz : 'Grębałowianka - Cracovia  wynik :'} , 
-    { mecz : 'Grębałowianka - Cracovia  wynik :'} , 
-    { mecz : 'Grębałowianka - Cracovia  wynik :'} , 
-    { mecz : 'Grębałowianka - Cracovia  wynik :'} , 
-    { mecz : 'Grębałowianka - Cracovia  wynik :'} , 
-    { mecz : 'Grębałowianka - Cracovia  wynik :'} , 
-    { mecz : 'Grębałowianka - Cracovia  wynik :'} , 
-    { mecz : 'Grębałowianka - Cracovia  wynik :'} , 
-    { mecz : 'Grębałowianka - Cracovia  wynik :'} , 
-  ]
+    { 
+      mecz : 'Grębałowianka - Cracovia  ' ,
+      wynik: '0:0' ,
+      statystyki : 'strzelcy :    asysty : '
+    } ,
+    
+    { 
+      mecz : 'Grębałowianka - Hutnik' ,
+      wynik: '0:0' ,
+      statystyki : 'strzelcy :    asysty : '
+    } , 
+
+    { 
+      mecz : 'Grębałowianka - Wisła' ,
+      wynik: '0:0' ,
+      statystyki : 'strzelcy :    asysty : '
+    } , 
+
+    { 
+      mecz : 'Grębałowianka - Borek' ,
+      wynik: '0:0' ,
+      statystyki : 'strzelcy :    asysty : '
+    } , 
+
+    { 
+      mecz : 'Grębałowianka - Dąbski' ,
+      wynik: '0:0' ,
+      statystyki : 'strzelcy :    asysty : '
+    } , 
+
+    { 
+      mecz : 'Grębałowianka - Opatkowianka' ,
+      wynik: '0:0' ,
+      statystyki : 'strzelcy :    asysty : '
+    } , 
+
+    { 
+      mecz : 'Grębałowianka - Garbarnia' ,
+      wynik: '0:0' ,
+      statystyki : 'strzelcy :    asysty : '
+    } , 
+
+    { 
+      mecz : 'Grębałowianka - Cracovia' ,
+      wynik: '0:0' ,
+      statystyki : 'strzelcy :    asysty : '
+    } , 
+
+    { 
+      mecz : 'Grębałowianka - Cracovia' ,
+      wynik: '0:0' ,
+      statystyki : 'strzelcy :    asysty : '
+    } , 
+
+
+    { 
+      mecz : 'Grębałowianka - Cracovia' ,
+      wynik: '0:0' ,
+      statystyki : 'strzelcy :    asysty : '
+    } , 
+
+]
+
   return (
-    <div>
+    <div className='wyniki-container'>
       <h1>Wyniki</h1>
-        <ul>
+        
          {wyniki.map( (item) => {
         return(
-          <li className='wynik'>{item.wynik}</li>
+          <div className='wynik'>
+            <h2>{item.mecz}</h2>
+            <h3>{item.wynik}</h3>
+            <h4>{item.statystyki}</h4>
+            </div>
         )
       })} 
-        </ul>
+        
       
 
     </div>
