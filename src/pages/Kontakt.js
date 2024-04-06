e0220326b066436f71611ecff8a9a87ae6806877
marcinoles87 <marcinoles87@gmail.com>
2024-04-06 21:57:04 +0200

    add pages and components

--- src/pages/Kontakt.js
+++ src/pages/Kontakt.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 function Kontakt() {
   return (
-    <div>Kontakt</div>
+    <div className='contact-container'>
+      <h1>Kontakt / Informacje</h1>
+      <p>Bernard Łach tel. </p>
+      <p>Piotr Kokosza tel. </p>
+
+      <h2>Informacje</h2>
+      <p>Oprócz meczy ligowych staramy sie spotykać i organizować gierki na orliku w Grębałowie.
+        Gramy po 6-8 osob w dwoch drużynach raz w tygodniu. Skladka za mecz po 10zł . Traktując to
+        jako " trening " staramy sie zgrywać i utrzymywać forme .
+      </p>
+
+      <p></p>
+      </div>
   )
 }
 
