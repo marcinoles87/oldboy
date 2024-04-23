c640d91e8b46effeb0978ddbe0425c9935ce37b5
marcinoles87 <marcinoles87@gmail.com>
2024-04-23 21:06:05 +0200

    add pages and components

--- src/pages/Historia.js
+++ src/pages/Historia.js
@@ -1,22 +1,37 @@
 import React from 'react'
 
+import his_img1 from '../img/2013.jpg'
+import his_img2 from '../img/2014_1.jpg'
+import his_img3 from '../img/2014_2.jpg'
+import his_img4 from '../img/2016_1.jpg'
+import his_img5 from '../img/2023.jpg'
+import his_img6 from '../img/hala.jpg'
+import his_img7 from '../img/2013.jpg'
+
 function Historia() {
   return (
     <div className='historia-container'>
       <h1>Grębałowianka oldboje</h1>
       <p>
 
+      <img src={his_img1} alt='oldboje2013'></img>
 Drużyna oldbojów w historii klubu występowała okazjonalnie z okazji
 jubileuszów i meczów noworocznych. Po raz pierwszy oficjalnie została
 zgłoszona do rozgrywek organizowanych przez Małopolski Związek Piłki
 Nożnej w 2013 roku. Rok później w 2014 roku Grębałowianka wywalczyła
 awans do I Krakowskiej Ligi Oldbojów na boisku z drugiego miejsca plasując
-się tuż za Orłem Piaski Wielkie. W tym samym roku rozegrała z okazji
+się tuż za Orłem Piaski Wielkie.
+<img src={his_img6} alt='oldboje2013'></img>
+ W tym samym roku rozegrała z okazji
 Jubileuszu 60 – lecia Klubu mecz z Hutnikiem Kraków lat 90-
 tych. W 2015 roku w rozgrywkach halowych uzyskała awans
-do I Krakowskiej Ligi Halowej. W 2016 roku została
+do I Krakowskiej Ligi Halowej.
+
+ W 2016 roku została
 wicemistrzem I Krakowskiej Ligi Oldbojów na boisku wyprzedzając drużyny
-oldboi Cracovii i Wisły Kraków. W latach 2013-2017 Grębałowiankę oldboje
+oldboi Cracovii i Wisły Kraków.
+<img src={his_img4} alt='oldboje2013'></img>
+ W latach 2013-2017 Grębałowiankę oldboje
 reprezentowali: Piotr Kokosza, Leszek Janiczak, Konrad Poplatek, Marcin
 Wierzbiński, Piotr Zięba, Łukasz Zawada, Dawid Bohun, Jarosław
 Krzyk, Jerzy Ostrowski, Bernard Łach, Grzegorz Luty, Piotr
@@ -31,6 +46,7 @@ Wojciech Sendor – kierownik, Sebastian Gregorczyk – kierownik,
 Łukasz Zawada – współsponsor, Andrzej Dziedzic – prezes, Zdzisław
 Wagner – prezes, Ryszard Chmiel – prezes, Grzegorz Sendor – wiceprezes,
 Piotr Zdziarski – wiceprezes.
+<img src={his_img2} alt='oldboje2013'></img>
 Najważniejszym sukcesem drużyny oldbojów jest możliwość kontynuacji
 pasji sportowej w dalszej przygodzie piłkarskiej z dobrymi kolegami i
 przyjaciółmi. Za każdy dar spotkania jesteśmy wdzięczni. Razem dla KSG.
@@ -40,6 +56,7 @@ III miejsce w Pucharze Ligi Krakowa na boisku, w 2021 roku III miejsce
 w Pucharze Ligi Krakowa na boisku, w 2022 roku Wicemistrzostwo
 I Krakowskiej Ligi Olbojów (pod balonem) <br></br>
 <br></br>
+<img src={his_img3} alt='oldboje2013'></img>
 Od 2015 roku drużyna K.S. Grębałowianka oldboje organizuje coroczny
 mecz charytatywny dla osób niepełnosprawnych goszcząc ich na trybunach,
 poczęstunkiem i wsparciem finansowym. Dla osób z niepełnosprawnością
@@ -56,6 +73,7 @@ oraz w 2004 roku z okazji Jubileuszu 50 - lecia klubu - trenerzy Bogdan Ślońsk
 Edward Bielewicz i Andrzej Sykta, Jubileuszu drużyny juniorów
 Leszka Janiczaka, meczów towarzyskich z Anglikami, które organizował
 Zdzisław Wagner.
+
 Od 2002 roku nasz klub 1 stycznia rozgrywa mecz noworoczny dopisując
 się do kilkudziesięcioletniej tradycji Cracovii. Głównie młodsi i starsi oldboje
 oraz wychowankowie klubu wzmocnieni chętnymi aktualnie seniorami
@@ -80,6 +98,7 @@ kierownik, Jerzy Ostrowski – II trener, Łukasz Zawada – współsponsor
 strojów, Piotr Zdziarski – prezes, Ryszard Chmiel – prezes, Tomasz Chmiel –
 wiceprezes, Mateusz Tabor – wiceprezes, Zbigniew Belski – wiceprezes,
 Stanisław Pituch – członek zarządu.</p>
+<img src={his_img5} alt='oldboje2013'></img>
       </div>
   )
 }
