c0c6bb071e676e10c70d848b51a44c5d9215371e
marcinoles87 <marcinoles87@gmail.com>
2024-05-31 08:35:44 +0200

    add new info

--- src/pages/Wydarzenia.js
+++ src/pages/Wydarzenia.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import wydarzenia1 from '../img/wydarzenia1.jpg'
 
 function Wydarzenia() {
   return (
@@ -8,6 +9,14 @@ function Wydarzenia() {
       <a href='https://www.facebook.com/Grebalowianka'>Grębałowianka oldboje od 2023 na stronie fb K.S. Grębałowianka</a>
       <a href='https://www.facebook.com/profile.php?id=100057690780345'>Grębałowianka oldboje w latach 2013-2023</a>
       <a href='https://www.facebook.com/profile.php?id=100064143066045'>Grębałowianka oldboje i mecze charytatywne</a>
+
+    <div className='wydarzenia-item'>
+      <h3>#70 lat KSG ! Wstęp wolny ! Zapraszamy !</h3>
+      <img src={wydarzenia1} alt='wydarzenia1'></img>
+    </div>
+
+  
+      
 
     </div>
   )
