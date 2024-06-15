1d1a422dc7561ff6a9c31e27f7399ec25bc4883a
marcinoles87 <marcinoles87@gmail.com>
2024-06-15 20:46:30 +0200

    add section

--- src/pages/opis/opisCharytatywny.js
+++ src/pages/opis/opisCharytatywny.js
@@ -1,12 +1,23 @@
 import React from 'react'
 
-import foto from '../opis/img/garbarnia.jpg'
+
+
+import f1 from '/web frontend/projekty React/oldboy/src/img/char2.jpg'
+import f2 from '/web frontend/projekty React/oldboy/src/img/char3.jpg'
+import f3 from '/web frontend/projekty React/oldboy/src/img/char4.jpg'
+import f4 from '/web frontend/projekty React/oldboy/src/img/char5.jpg'
+import f5 from '/web frontend/projekty React/oldboy/src/img/charytatywny.jpg'
 
 function opisCharytatywny() {
   return (
     <>
-    <div>Charytatywny</div>
-    <img src={foto} alt='d'></img>
+    <div className='charytatywny-container'> Mecz Charytatywny 2024
+    <img src={f5} alt='d'></img>
+    <img src={f2} alt='d'></img>
+    <img src={f3} alt='d'></img>
+    <img src={f4} alt='d'></img>
+    <img src={f1} alt='d'></img>
+    </div>
 
     </>
   )
