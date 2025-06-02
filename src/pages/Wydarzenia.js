ec4815edf90eb78d30de05f8ff5c6fdd99d98493
marcinoles87 <marcinoles87@gmail.com>
2025-06-02 16:49:21 +0200

    add new score

--- src/pages/Wydarzenia.js
+++ src/pages/Wydarzenia.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import wydarzenia1 from '../img/wydarzenia1.jpg'
 import wydarzenia2 from '../img/wydarzenia2.jpg'
+import charytatywny2025 from '../img/charytatywny2025.jpg'
 import { Link } from 'react-router-dom'
 
 function Wydarzenia() {
@@ -12,6 +13,11 @@ function Wydarzenia() {
       <a href='https://www.facebook.com/profile.php?id=100057690780345'>Grębałowianka oldboje w latach 2013-2023</a>
       <a href='https://www.facebook.com/profile.php?id=100064143066045'>Grębałowianka oldboje i mecze charytatywne</a>
       <Link to={'/opis/opisCharytatywny'}>Mecz charytatywny 8.06.2024</Link>
+
+       <div className='wydarzenia-item'>
+      <h3>Mecz charytatywny 7.06.2025</h3>
+      <img src={charytatywny2025} alt='wydarzenia1'></img>
+    </div>
 
     <div className='wydarzenia-item'>
       <h3>#70 lat KSG ! Wstęp wolny ! Zapraszamy !</h3>
